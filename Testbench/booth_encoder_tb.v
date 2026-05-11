3131017629007feb12c14c474913d954bcb815b7
Dhathri P G <dhathripg21@gmail.com>
2026-05-12 01:06:10 +0530

    Add testbench for booth_multiplier module

--- Testbench/booth_encoder_tb.v
+++ Testbench/booth_encoder_tb.v
@@ -1,1 +1,13 @@
-
+module booth_multiplier_tb;
+reg x1, x0;
+wire [1:0] y;
+booth_multiplier uut(y, x1, x0);
+initial 
+begin
+  x1=0; x0=0;
+  #10 x1=0; x0=1;
+  #10 x1=1; x0=0;
+  #10 x1=1; x0=1;
+  #10 $finish;
+end
+endmodule
