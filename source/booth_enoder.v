ddf9e15b07525e6cd33c13dbd43a6f7f23a585f9
Dhathri P G <dhathripg21@gmail.com>
2026-05-12 01:04:38 +0530

    Update booth_enoder.v

--- source/booth_enoder.v
+++ source/booth_enoder.v
@@ -1,1 +1,10 @@
-
+module booth_multiplier(output reg [1:0] y, input x1, x0);
+always @(*) begin
+  case({x1,x0})
+    2'b00: y = 0;
+    2'b01: y = 1;
+    2'b10: y = 2;
+    2'b11: y = 0;
+  endcase
+end
+endmodule
