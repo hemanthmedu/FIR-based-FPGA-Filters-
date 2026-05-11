fb86c110c53e75b786e3ca7d47dd30b8b1e3bb4f
DeekshaK06 <deekshakandiga@gmail.com>
2026-05-12 01:30:51 +0530

    Update shifter_tb.v

--- Testbench/shifter_tb.v
+++ Testbench/shifter_tb.v
@@ -1,1 +1,24 @@
+module shifter_tb();
 
+reg [7:0] a;
+reg sel;
+wire [7:0] y;
+
+shifter uut (.a(a),.sel(sel),.y(y));
+
+initial 
+  begin
+
+    a = 8'b00000101;
+
+    sel = 0;   
+    #10;
+
+    sel = 1;  
+    #10;
+
+    $finish;
+
+end
+
+endmodule
