b5e83824695253cf2f313a716569d88daa9aaccf
DeekshaK06 <deekshakandiga@gmail.com>
2026-05-12 00:06:53 +0530

    Update coeff_rom_tb.v

--- Testbench/coeff_rom_tb.v
+++ Testbench/coeff_rom_tb.v
@@ -1,1 +1,19 @@
+module coeff_rom_tb();
 
+reg [2:0] addr;
+wire [11:0] data;
+
+coeff_rom uut (.addr(addr), .data(data));
+
+integer i;
+
+initial begin
+    $monitor("addr=%b data=%d", addr, data);
+    for(i=0; i<8; i=i+1) begin
+        addr = i; 
+        #10;
+    end
+    $finish;
+end
+
+endmodule
