94f27a49a21806cd67601f6de39137a27f75ba05
Dhathri P G <dhathripg21@gmail.com>
2026-05-27 12:12:27 +0530

    Refactor coeff_rom module to use signed output

--- source/coeff_rom.v
+++ source/coeff_rom.v
@@ -1,18 +1,27 @@
-module coeff_rom (
+module coeff_rom(
     input [2:0] addr,
-    output reg [11:0] data
+    output reg signed [11:0] coeff
 );
+
 always @(*) begin
+
     case(addr)
-        3'd0: data = 12'd10;
-        3'd1: data = 12'd20;
-        3'd2: data = 12'd30;
-        3'd3: data = 12'd40;
-        3'd4: data = 12'd50;
-        3'd5: data = 12'd40;
-        3'd6: data = 12'd30;
-        3'd7: data = 12'd20;
-        default: data = 0;
+
+        3'd0: coeff = 12'sd10;
+        3'd1: coeff = 12'sd20;
+        3'd2: coeff = 12'sd30;
+        3'd3: coeff = 12'sd40;
+
+        3'd4: coeff = 12'sd50;
+
+        3'd5: coeff = 12'sd40;
+        3'd6: coeff = 12'sd30;
+        3'd7: coeff = 12'sd20;
+
+        default: coeff = 12'sd0;
+
     endcase
+
 end
+
 endmodule
