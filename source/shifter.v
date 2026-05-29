27063eae300990da3fe132d2153651815df97c23
DeekshaK06 <deekshakandiga@gmail.com>
2026-05-29 22:40:12 +0530

    Refactor shifter module for signed 27-bit operations
    Updated shifter module to handle signed 27-bit inputs and outputs.

--- source/shifter.v
+++ source/shifter.v
@@ -1,10 +1,24 @@
-module shifter(input [7:0] a,sel,output reg [7:0] y);
-always @(*) begin
+module shifter(
+
+    input signed [27:0] a,
+
+    input sel,
+
+    output reg signed [27:0] y
+
+);
+
+always @(*)
+
+begin
 
     if(sel)
-        y = a << 1;   
+
+        y = a <<< 1;
+
     else
-        y = a >> 1;   
+
+        y = a >>> 1;
 
 end
 
