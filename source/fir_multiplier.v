39c4b4bc614de5795ae673abe74f18da7aa0589e
DeekshaK06 <deekshakandiga@gmail.com>
2026-05-29 22:30:55 +0530

    Update and rename fir_multiplier to fir_multiplier.v

---
+++ source/fir_multiplier.v
@@ -0,0 +1,13 @@
+
+module fir_multiplier(
+
+    input signed [11:0] sample,
+    input signed [11:0] coeff,
+
+    output signed [23:0] product
+
+);
+
+assign product = sample * coeff;
+
+endmodule
