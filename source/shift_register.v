4057804873a75ec1925ac2bfadfcab73ff95de09
DeekshaK06 <deekshakandiga@gmail.com>
2026-05-29 22:37:27 +0530

    Refactor delay_reg_12bit to shift_reg_12bit

--- source/shift_register.v
+++ source/shift_register.v
@@ -1,17 +1,57 @@
-module delay_reg_12bit(
+module shift_reg_12bit(
+
     input clk,
     input rst,
     input en,
+
     input signed [11:0] din,
-    output reg signed [11:0] dout
+
+    output reg signed [11:0] q0,
+    output reg signed [11:0] q1,
+    output reg signed [11:0] q2,
+    output reg signed [11:0] q3,
+    output reg signed [11:0] q4,
+    output reg signed [11:0] q5,
+    output reg signed [11:0] q6,
+    output reg signed [11:0] q7
+
 );
 
-always @(posedge clk) begin
+always @(posedge clk)
+
+begin
+
     if(rst)
-        dout <= 12'd0;
+
+    begin
+
+        q0 <= 0;
+        q1 <= 0;
+        q2 <= 0;
+        q3 <= 0;
+        q4 <= 0;
+        q5 <= 0;
+        q6 <= 0;
+        q7 <= 0;
+
+    end
 
     else if(en)
-        dout <= din;
+
+    begin
+
+        q7 <= q6;
+        q6 <= q5;
+        q5 <= q4;
+        q4 <= q3;
+        q3 <= q2;
+        q2 <= q1;
+        q1 <= q0;
+
+        q0 <= din;
+
+    end
+
 end
 
 endmodule
