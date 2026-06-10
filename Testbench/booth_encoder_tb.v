8978ee4ef93c8e2ba64aceedeb0e2df980294b50
DeekshaK06 <deekshakandiga@gmail.com>
2026-06-10 20:33:40 +0530

    Update booth_encoder_tb.v

--- Testbench/booth_encoder_tb.v
+++ Testbench/booth_encoder_tb.v
@@ -1,13 +1,62 @@
-module booth_multiplier_tb;
-reg x1, x0;
-wire [1:0] y;
-booth_multiplier uut(y, x1, x0);
-initial 
-begin
-  x1=0; x0=0;
-  #10 x1=0; x0=1;
-  #10 x1=1; x0=0;
-  #10 x1=1; x0=1;
-  #10 $finish;
+`timescale 1ns/1ps
+
+module booth_encoder_tb;
+
+reg [11:0] coeff;
+
+wire [1:0] booth_code0;
+wire [1:0] booth_code1;
+wire [1:0] booth_code2;
+wire [1:0] booth_code3;
+wire [1:0] booth_code4;
+wire [1:0] booth_code5;
+
+booth_encoder DUT(
+
+    .coeff(coeff),
+
+    .booth_code0(booth_code0),
+    .booth_code1(booth_code1),
+    .booth_code2(booth_code2),
+    .booth_code3(booth_code3),
+    .booth_code4(booth_code4),
+    .booth_code5(booth_code5)
+
+);
+
+initial begin
+
+    coeff = 12'b000000000000;
+    #10;
+
+    coeff = 12'b010101010101;
+    #10;
+
+    coeff = 12'b111100001111;
+    #10;
+
+    coeff = 12'b101010101010;
+    #10;
+
+    coeff = 12'b111111111111;
+    #10;
+
+    $finish;
+
 end
+
+initial begin
+
+    $monitor(
+    "coeff=%b bc5=%b bc4=%b bc3=%b bc2=%b bc1=%b bc0=%b",
+     coeff,
+     booth_code5,
+     booth_code4,
+     booth_code3,
+     booth_code2,
+     booth_code1,
+     booth_code0);
+
+end
+
 endmodule
