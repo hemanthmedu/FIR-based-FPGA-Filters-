06c4bad222dc7a7f1e455093b2d6fe2b449241d6
DeekshaK06 <deekshakandiga@gmail.com>
2026-06-10 20:20:27 +0530

    Refactor and_gate_tb module and test cases

--- Testbench/and_gate_tb.v
+++ Testbench/and_gate_tb.v
@@ -1,17 +1,16 @@
-module and_gate_tb();
-reg a, b;
+module and_gate_tb;
+
+reg a,b;
 wire y;
-and_gate uut (.a(a),.b(b),.y(y));
 
-initial 
-  begin
+and_gate DUT(.a(a),.b(b),.y(y));
 
-    a = 0; b = 0; #10;
-    a = 0; b = 1; #10;
-    a = 1; b = 0; #10;
-    a = 1; b = 1; #10;
-
-    $finish;
+initial begin
+    a=0;b=0;
+    #10 a=0;b=1;
+    #10 a=1;b=0;
+    #10 a=1;b=1;
+    #10 $finish;
 end
 
 endmodule
