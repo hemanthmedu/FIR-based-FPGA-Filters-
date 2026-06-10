b8ff97988ff4d1d170c1e05b527a71cd613fe1e4
DeekshaK06 <deekshakandiga@gmail.com>
2026-06-10 20:25:10 +0530

    Refactor shift register testbench for 12-bit module

--- Testbench/shift_register_tb.v
+++ Testbench/shift_register_tb.v
@@ -1,30 +1,43 @@
-module shift_reg_tb;
+module shift_reg_12bit_tb;
 
-reg clk, rst, en, din;
-wire dout;
+reg clk,rst,en;
+reg signed [11:0] din;
 
-shift_reg_12bit uut (.clk(clk),.en(en),.rst(rst),.din(din),.dout(dout));
-always #5 clk = ~clk;
-initial 
-begin
-    clk = 0; rst = 1; en = 0; din = 0;
+wire signed [11:0] q0,q1,q2,q3,q4,q5,q6,q7;
 
-    #10 rst = 0; en = 1;
+shift_reg_12bit DUT(
+    .clk(clk),
+    .rst(rst),
+    .en(en),
+    .din(din),
+    .q0(q0),
+    .q1(q1),
+    .q2(q2),
+    .q3(q3),
+    .q4(q4),
+    .q5(q5),
+    .q6(q6),
+    .q7(q7)
+);
 
-    din = 1; #10;
-    din = 0; #10;
-    din = 1; #10;
-    din = 1; #10;
-    din = 0; #10;
-    din = 1; #10;
-    din = 1; #10;
-    din = 0; #10;
-    din = 1; #10;
-    din = 1; #10;
-    din = 0; #10;
-    din = 1; #10;
+always #5 clk=~clk;
 
-    #50 $stop;
+initial begin
+    clk=0;
+    rst=1;
+    en=0;
+    din=0;
+
+    #10 rst=0;
+    en=1;
+
+    din=12'd1; #10;
+    din=12'd2; #10;
+    din=12'd3; #10;
+    din=12'd4; #10;
+    din=12'd5; #10;
+
+    #20 $finish;
 end
 
 endmodule
