0ba6224fb1291494e575c72a27d1646abda509d2
DeekshaK06 <deekshakandiga@gmail.com>
2026-06-10 20:26:11 +0530

    Refactor xor_gate_tb for improved readability

--- Testbench/xor_gate_tb.v
+++ Testbench/xor_gate_tb.v
@@ -1,13 +1,20 @@
 module xor_gate_tb;
-reg a, b;
+
+reg a,b;
 wire y;
-xor_gate uut(y, a, b);
-initial 
-begin
-  a=0; b=0;
-  #10 a=0; b=1;
-  #10 a=1; b=0;
-  #10 a=1; b=1;
-  #10 $finish;
+
+xor_gate DUT(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a=0;b=0;
+    #10 a=0;b=1;
+    #10 a=1;b=0;
+    #10 a=1;b=1;
+    #10 $finish;
 end
+
 endmodule
