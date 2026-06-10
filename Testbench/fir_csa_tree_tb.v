29ee1dd28f748c359fb12824a0cb84f28d48ebdf
DeekshaK06 <deekshakandiga@gmail.com>
2026-06-10 20:29:46 +0530

    Create booth_multiplier testbench
    Added testbench for booth_multiplier with various sample and coefficient values.

--- Testbench/fir_csa_tree_tb.v
+++ Testbench/fir_csa_tree_tb.v
@@ -1,12 +1,57 @@
-module booth_multiplier(
+`timescale 1ns/1ps
 
-    input  signed [11:0] sample,
-    input  signed [11:0] coeff,
+module booth_multiplier_tb;
 
-    output signed [23:0] product
+reg signed [11:0] sample;
+reg signed [11:0] coeff;
+
+wire signed [23:0] product;
+
+booth_multiplier DUT(
+
+    .sample(sample),
+    .coeff(coeff),
+    .product(product)
 
 );
 
-assign product = sample * coeff;
+initial
+begin
+
+    $display("Time\tSample\tCoeff\tProduct");
+
+    sample = 12'sd10;
+    coeff  = 12'sd20;
+    #10;
+    $display("%0t\t%d\t%d\t%d",$time,sample,coeff,product);
+
+    sample = 12'sd25;
+    coeff  = 12'sd4;
+    #10;
+    $display("%0t\t%d\t%d\t%d",$time,sample,coeff,product);
+
+    sample = -12'sd15;
+    coeff  = 12'sd8;
+    #10;
+    $display("%0t\t%d\t%d\t%d",$time,sample,coeff,product);
+
+    sample = 12'sd12;
+    coeff  = -12'sd5;
+    #10;
+    $display("%0t\t%d\t%d\t%d",$time,sample,coeff,product);
+
+    sample = -12'sd10;
+    coeff  = -12'sd10;
+    #10;
+    $display("%0t\t%d\t%d\t%d",$time,sample,coeff,product);
+
+    sample = 12'sd0;
+    coeff  = 12'sd100;
+    #10;
+    $display("%0t\t%d\t%d\t%d",$time,sample,coeff,product);
+
+    #10 $finish;
+
+end
 
 endmodule
