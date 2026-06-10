7281ebef083f19c586fc0474bfbf79c99b56ae1e
DeekshaK06 <deekshakandiga@gmail.com>
2026-06-10 20:29:10 +0530

    Refactor testbench to implement booth multiplier

--- Testbench/fir_csa_tree_tb.v
+++ Testbench/fir_csa_tree_tb.v
@@ -1,24 +1,12 @@
-module fir_csa_tree_tb;
+module booth_multiplier(
 
-reg [23:0] M0,M1,M2,M3,M4,M5,M6,M7;
-wire [27:0] FINAL_OUT;
+    input  signed [11:0] sample,
+    input  signed [11:0] coeff,
 
-fir_csa_tree DUT(
-    .M0(M0),.M1(M1),.M2(M2),.M3(M3),
-    .M4(M4),.M5(M5),.M6(M6),.M7(M7),
-    .FINAL_OUT(FINAL_OUT)
+    output signed [23:0] product
+
 );
 
-initial begin
-    M0=10; M1=20; M2=30; M3=40;
-    M4=50; M5=60; M6=70; M7=80;
-
-    #20;
-
-    M0=1; M1=2; M2=3; M3=4;
-    M4=5; M5=6; M6=7; M7=8;
-
-    #20 $finish;
-end
+assign product = sample * coeff;
 
 endmodule
